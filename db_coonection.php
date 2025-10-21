d5102ffd6058e1a7fac4e3c63e4d1a83681c4e0d
rishmiyareets <rishma352@gmail.com>
2025-10-21 20:29:33 +0530

    create and code db_conntion.php file

---
+++ db_coonection.php
@@ -0,0 +1,15 @@
+<?php
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "Rif25677";
+$dbname = "matlogix_db";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
